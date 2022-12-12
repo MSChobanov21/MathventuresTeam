a22d10e854b509193a3582b581fec2edccdf248a
Sergey Toptunov <SAToptunov21@codingburgas.bg>
2022-12-12 22:35:12 +0200

    Input for four digits :D

--- MathventuresTeam/MathventuresTeam.cpp
+++ MathventuresTeam/MathventuresTeam.cpp
@@ -20,4 +20,17 @@ int main()
         d[2] = rand() % 10;
         d[3] = rand() % 10;
     } while ((d[0] == d[1]) or (d[0] == d[2]) or (d[0] == d[3]) or (d[1] == d[2]) or (d[1] == d[3]) or (d[2] == d[3]));
+    
+    cout << "You're playing Bulls and Cows" << endl << "You must guess the 4 digits number within 15 tries!" << endl;
+    for (int i = 15; i != 0; i--) {
+        bull = 0;
+        cow = 0;
+        cout << endl << "You've got " << i << " tries left" << endl << "Enter the 4 unique digits:";
+        cin >> gNum;
+        g[0] = gNum / 1000;
+        g[1] = gNum / 100 - g[0] * 10;
+        g[2] = gNum / 10 - g[0] * 100 - g[1] * 10;
+        g[3] = gNum % 10;
+        cout << g[0] << " " << g[1] << " " << g[2] << " " << g[3] << endl;
+    }
 }
